8d46d7fa91d25ca352ec6ee7f36797572a2a78f4
MohammadMustafawaqar <mustafawaqar488@gmail.com>
2024-11-13 15:55:26 +0430

    update on edit fucking issue

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="{{ asset('assets/plugins/bootstrap-rtl/bootstrap-rtl.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/plugins/bootstrap-rtl/bootstrap-rtl.min.css') }}">
     <style>
         /* Global Styles */
         * {
